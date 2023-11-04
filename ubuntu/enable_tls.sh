53b380dcf28a63cc0b1071b5519a8c63899ac260
Anil Kunwar <107930100+anilkunwar421@users.noreply.github.com>
2023-11-05 03:20:32 +0545

    Update enable_tls.sh

--- ubuntu/enable_tls.sh
+++ ubuntu/enable_tls.sh
@@ -12,7 +12,7 @@ if [ "$DOMAIN" = "yes" ]; then
     read -p "Enter Your or your company domain (eg: example.com): " DOMAIN_NAME
     with_dns="DNS:$DOMAIN_NAME"
 else
-    read -p "Enter Your or replica ip address (eg: 192.168.0.1): " DOMAIN_NAME
+    read -p "Enter replica ip address (eg: 192.168.0.1): " DOMAIN_NAME
     with_dns="IP:$DOMAIN_NAME"
 fi
 if [ "$FIRST_REPLICA" = "no" ]; then
@@ -36,6 +36,11 @@ sudo chmod 700 /etc/mongodb-certificates
 echo "Creating a Certificate Authority..."
 if [ "$FIRST_REPLICA" = "yes" ]; then
     sudo openssl req -newkey rsa:4096 -nodes -keyout /etc/mongodb-certificates/mongodb.key -x509 -days 365 -out /etc/mongodb-certificates/mongodb.crt
+    # Ensure the permissions are correct
+    chmod 600 /etc/mongodb-certificates/mongodb.key
+    chmod 600 /etc/mongodb-certificates/mongodb.crt
+    sudo chown mongodb:mongodb /etc/mongodb-certificates/mongodb.key
+    sudo chown mongodb:mongodb /etc/mongodb-certificates/mongodb.crt
 else
     sshpass -p "$FIRST_REPLICA_PASSWORD" scp -o StrictHostKeyChecking=no root@"$FIRST_REPLICA_IP":/etc/mongodb-certificates/mongodb.key /etc/mongodb-certificates/mongodb.key
     sshpass -p "$FIRST_REPLICA_PASSWORD" scp -o StrictHostKeyChecking=no root@"$FIRST_REPLICA_IP":/etc/mongodb-certificates/mongodb.crt /etc/mongodb-certificates/mongodb.crt
@@ -52,6 +57,11 @@ sudo openssl genrsa -out /etc/mongodb-certificates/$DOMAIN_NAME.key 4096
 sudo openssl req -new -key /etc/mongodb-certificates/$DOMAIN_NAME.key -out /etc/mongodb-certificates/$DOMAIN_NAME.csr \
 -subj "/C=$COUNTRY_CODE/ST=$COMPANY_STATE/L=$COMPANY_CITY/O=$COMPANY_NAME/emailAddress=$EMAIL_ADDRESS/CN=$DOMAIN_NAME" \
 -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=$with_dns"))
+# Ensure the permissions are correct
+chmod 600 /etc/mongodb-certificates/$DOMAIN_NAME.key
+chmod 600 /etc/mongodb-certificates/$DOMAIN_NAME.csr
+sudo chown mongodb:mongodb /etc/mongodb-certificates/$DOMAIN_NAME.key
+sudo chown mongodb:mongodb /etc/mongodb-certificates/$DOMAIN_NAME.csr
 
 # Sign the CSR with your CA
 echo "Signing the CSR with the CA..."
@@ -62,6 +72,9 @@ sudo openssl x509 -req -in /etc/mongodb-certificates/$DOMAIN_NAME.csr -CA /etc/m
 # Create the .pem file
 echo "Creating .pem file..."
 sudo cat /etc/mongodb-certificates/$DOMAIN_NAME.key /etc/mongodb-certificates/$DOMAIN_NAME.crt > /etc/mongodb-certificates/$DOMAIN_NAME.pem
+# Ensure the permissions are correct
+chmod 600 /etc/mongodb-certificates/$DOMAIN_NAME.pem
+sudo chown mongodb:mongodb /etc/mongodb-certificates/$DOMAIN_NAME.pem
 
 # Update the MongoDB configuration file
 echo "Updating the /etc/mongod.conf file with domain name $DOMAIN_NAME..."
