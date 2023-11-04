04aa7b4620841bd366ace8e29fed148d9ae8915c
Anil Kunwar <107930100+anilkunwar421@users.noreply.github.com>
2023-11-05 04:32:13 +0545

    Update dns_setup.sh

--- ubuntu/dns_setup.sh
+++ ubuntu/dns_setup.sh
@@ -1,70 +1,91 @@
 #!/bin/bash
 
 # Prompt user for input
-read -p "Name of replica set (e.g., company_replicaset): " name
-read -p "Replica 1 IP: " replica1_ip
-read -p "Replica 2 IP: " replica2_ip
-read -p "Replica 3 IP: " replica3_ip
-read -p "Is this the last replica you are updating? (Yes/No): " last_replica
-read -p "Current replica domain (optional): " replica_identifier
+read -p "Enter Your or your company country code (eg: US, CA): " COUNTRY_CODE
+read -p "Enter Your or your company state (eg: California): " COMPANY_STATE
+read -p "Enter Your or your company city (eg: San Francisco): " COMPANY_CITY
+read -p "Enter Your or your company name (eg: MongoDB): " COMPANY_NAME
+read -p "Enter Your or your company email address: " EMAIL_ADDRESS
+read -p "do you have domain for these replica (yes/no): " DOMAIN
+read -p "is this your first replica (yes/no): " FIRST_REPLICA
+if [ "$DOMAIN" = "yes" ]; then
+    read -p "Enter Your or your company domain (eg: example.com): " DOMAIN_NAME
+    with_dns="DNS:$DOMAIN_NAME"
+else
+    read -p "Enter replica ip address (eg: 192.168.0.1): " DOMAIN_NAME
+    with_dns="IP:$DOMAIN_NAME"
+fi
+if [ "$FIRST_REPLICA" = "no" ]; then
+    read -p "Enter IP of first replica: " FIRST_REPLICA_IP
+    read -p "Enter Password of first replica: " FIRST_REPLICA_PASSWORD
+fi
+# Update the system
+echo "Updating the system..."
+sudo apt-get update -y
 
-# Check if replica_identifier is empty and ask for IP
-if [ -z "$replica_identifier" ]; then
-    read -p "Current replica IP: " replica_identifier
+# Create the new directory for MongoDB certificates
+echo "Creating /etc/mongodb-certificates directory..."
+sudo mkdir -p /etc/mongodb-certificates
+
+# Set the ownership and permissions for the directory
+echo "Setting permissions for /etc/mongodb-certificates..."
+sudo chown mongodb:mongodb /etc/mongodb-certificates
+sudo chmod 700 /etc/mongodb-certificates
+
+# Create a Certificate Authority
+echo "Creating a Certificate Authority..."
+if [ "$FIRST_REPLICA" = "yes" ]; then
+    sudo openssl req -new -x509 -days 365 -keyout /etc/mongodb-certificates/mongodb.key -out /etc/mongodb-certificates/mongodb.crt \
+    -subj "/C=$COUNTRY_CODE/ST=$COMPANY_STATE/L=$COMPANY_CITY/O=$COMPANY_NAME/emailAddress=$EMAIL_ADDRESS/CN=$DOMAIN_NAME"
+    
+    # Ensure the permissions are correct
+    chmod 600 /etc/mongodb-certificates/mongodb.key
+    chmod 600 /etc/mongodb-certificates/mongodb.crt
+    sudo chown mongodb:mongodb /etc/mongodb-certificates/mongodb.key
+    sudo chown mongodb:mongodb /etc/mongodb-certificates/mongodb.crt
+else
+    sshpass -p "$FIRST_REPLICA_PASSWORD" scp -o StrictHostKeyChecking=no root@"$FIRST_REPLICA_IP":/etc/mongodb-certificates/mongodb.key /etc/mongodb-certificates/mongodb.key
+    sshpass -p "$FIRST_REPLICA_PASSWORD" scp -o StrictHostKeyChecking=no root@"$FIRST_REPLICA_IP":/etc/mongodb-certificates/mongodb.crt /etc/mongodb-certificates/mongodb.crt
+    # Ensure the permissions are correct
+    chmod 600 /etc/mongodb-certificates/mongodb.key
+    chmod 600 /etc/mongodb-certificates/mongodb.crt
+    sudo chown mongodb:mongodb /etc/mongodb-certificates/mongodb.key
+    sudo chown mongodb:mongodb /etc/mongodb-certificates/mongodb.crt
 fi
 
-if [ "$last_replica" = "Yes" ]; then
-    read -p "Enter mongodb admin password: " ADMIN_PWD
-    read -p "Enter 1st replica domain (optional): " first_replica_domain
-    read -p "Enter 2nd replica domain (optional): " second_replica_domain
-    read -p "Enter 3rd replica domain (optional): " third_replica_domain
-fi
+# Create node-specific key and CSR
+echo "Creating node-specific key and CSR..."
+sudo openssl genrsa -out /etc/mongodb-certificates/$DOMAIN_NAME.key 4096
+sudo openssl req -new -key /etc/mongodb-certificates/$DOMAIN_NAME.key -out /etc/mongodb-certificates/$DOMAIN_NAME.csr \
+-subj "/C=NP/ST=Gandaki/L=Pokhara/O=test/emailAddress=test@example.com/CN=$DOMAIN_NAME" \
+-reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=$with_dns"))
 
-# Update UFW rules
-echo "y" | sudo ufw enable
-sudo ufw allow 22
-sudo ufw allow from $replica1_ip to any port 27017
-sudo ufw allow from $replica2_ip to any port 27017
-sudo ufw allow from $replica3_ip to any port 27017
-sudo ufw reload
 
-# Update bindIp with the current replica identifier
-sudo sed -i "/  bindIp:/s/127.0.0.1/&,$replica_identifier/" /etc/mongod.conf
+# Ensure the permissions are correct
+chmod 600 /etc/mongodb-certificates/$DOMAIN_NAME.key
+chmod 600 /etc/mongodb-certificates/$DOMAIN_NAME.csr
+sudo chown mongodb:mongodb /etc/mongodb-certificates/$DOMAIN_NAME.key
+sudo chown mongodb:mongodb /etc/mongodb-certificates/$DOMAIN_NAME.csr
 
-# Uncomment and set the replica set name
-sudo sed -i "s/#replication:/replication:/" /etc/mongod.conf
-sudo sed -i '/^replication:/a \ \ replSetName: '"$name"'' /etc/mongod.conf
+# Sign the CSR with your CA
+echo "Signing the CSR with the CA..."
+sudo openssl x509 -req -in $DOMAIN_NAME.csr -CA mongodb.crt -CAkey mongodb.key -CAcreateserial -out $DOMAIN_NAME.crt -days 365 -extfile <(printf "subjectAltName=$with_dns")
 
-# Restart MongoDB to apply changes
+# Create the .pem file
+echo "Creating .pem file..."
+sudo cat /etc/mongodb-certificates/$DOMAIN_NAME.key /etc/mongodb-certificates/$DOMAIN_NAME.crt > /etc/mongodb-certificates/$DOMAIN_NAME.pem
+# Ensure the permissions are correct
+chmod 600 /etc/mongodb-certificates/$DOMAIN_NAME.pem
+sudo chown mongodb:mongodb /etc/mongodb-certificates/$DOMAIN_NAME.pem
+
+# Update the MongoDB configuration file
+echo "Updating the /etc/mongod.conf file with domain name $DOMAIN_NAME..."
+sudo sed -i "/^net:/a \ \ tls:\n\ \ \ \ mode: requireTLS\n\ \ \ \ certificateKeyFile: /etc/mongodb-certificates/$DOMAIN_NAME.pem\n\ \ \ \ CAFile: /etc/mongodb-certificates/mongodb.crt" /etc/mongod.conf
+sudo sed -i "/^net:/a \ \ maxIncomingConnections: 999999" /etc/mongod.conf
+# Restart and check the status of MongoDB
+echo "Restarting MongoDB and checking the status..."
 sudo systemctl restart mongod
+sleep 10
+sudo systemctl status mongod --no-pager
 
-# If this is the last replica, configure replica set
-if [ "$last_replica" = "Yes" ]; then
-    # Wait for MongoDB to restart
-    sleep 10
-    
-    # Check if MongoDB is ready
-    until echo 'db.runCommand({ ping: 1 })' | mongosh --port 27017 -u "mongo_admin" -p "$ADMIN_PWD" --authenticationDatabase admin --quiet; do
-        sleep 5
-    done
-    
-    # Execute the replica set initiation
-    output=$(echo "rs.initiate({
-        _id: '$name',
-        members: [
-            { _id: 0, host: '$first_member' },
-            { _id: 1, host: '$second_member' },
-            { _id: 2, host: '$third_member' }
-        ]
-    });" | mongosh --port 27017 -u "mongo_admin" -p "$ADMIN_PWD" --authenticationDatabase admin --quiet 2>&1)
-    
-    # Check the output for success or failure
-    if echo "$output" | grep -q '"ok" : 1'; then
-        echo "Replica set configured successfully."
-    else
-        echo "Failed to configure replica set:"
-        echo "$output"
-    fi
-fi
-
-
+echo "Script execution completed!"
